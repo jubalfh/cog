e8f4f8fc513ed887756b8a19a430bdb2df081255
Miroslaw Baran <miroslaw+p+cog@makabra.org>
2015-05-19 13:08:22 +0100

    Assorted fixes (readability mostly).

--- cog/util.py
+++ cog/util.py
@@ -8,15 +8,14 @@
 # miscellaneous utility functions, mostly borrowed
 
 import os
-import sys
 import pwd
 import string
-import re
 import random
 import keyring
 import getpass
 
 from passlib.hash import sha512_crypt
+
 
 def randomized_string(size=16, chars=string.letters + string.digits + string.punctuation):
     """
@@ -26,12 +25,15 @@ def randomized_string(size=16, chars=string.letters + string.digits + string.pun
     """
     return ''.join(random.choice(chars) for x in range(size))
 
+
 def make_pass(passwd=None):
-    # generate password using crypt()'s SHA-512 method, randomized salt and
-    # randomized number of rounds.
+    """
+    generate password using SHA-512 method, randomized salt and randomized
+    number of rounds.
+    """
     if passwd is None:
         passwd = randomized_string(17)
-    salt = randomized_string(16, ( './' + string.letters + string.digits))
+    salt = randomized_string(16, ('./' + string.letters + string.digits))
     iterations = random.randint(40000, 80000)
     return '{CRYPT}' + sha512_crypt.encrypt(passwd, salt=salt, rounds=iterations)
 
@@ -56,11 +58,13 @@ def get_pass(username, service, prompt, use_keyring=False):
 def get_current_uid():
     return pwd.getpwuid(os.getuid()).pw_name
 
+
 def flatten_list(messy_list):
     """
     Flatten an unruly list of lists. Cf. <http://stackoverflow.com/a/952914>
     """
     return [item for sublist in messy_list for item in sublist]
+
 
 def merge(d1, d2):
     """
@@ -68,7 +72,7 @@ def merge(d1, d2):
     dictionary at the same positions too (with caveats).
     """
     for k1, v1 in d1.iteritems():
-        if not k1 in d2:
+        if k1 not in d2:
             d2[k1] = v1
         elif isinstance(v1, list):
             d2[k1] = list(set(d2[k1] + v1))
@@ -76,10 +80,11 @@ def merge(d1, d2):
             merge(v1, d2[k1])
     return d2
 
+
 class Singleton(type):
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
-
